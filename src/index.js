077da52f6de750ed9db47464b734727569c70178
banana <banana@eurus.cn>
2018-12-08 17:33:07 +0800

    add djs

--- src/index.js
+++ src/index.js
@@ -1,15 +1,26 @@
 import './index.scss'
 import m from 'mithril'
+import djs from 'dayjs'
 const root = document.querySelector('.app')
 
+var Data = {
+    current_pvt: djs(),
+    current_week_start: djs().startOf('week'),
+    current_week_end: djs().endOf('week'),
+    lists: [
+        djs().startOf('week'),
+        djs().startOf('week').add(1, 'day'),
+        djs().startOf('week').add(2, 'day'),
+        djs().startOf('week').add(3, 'day'),
+        djs().startOf('week').add(4, 'day'),
+        djs().startOf('week').add(5, 'day'),
+        djs().startOf('week').add(6, 'day')
+    ]
+}
+console.log(Data.current_week_end.diff(Data.current_week_start, 'day'))
 const Card = {
     view: function(vnode) {
         return m('div.card', [
-            m('p', "Hello Card"),
-            m('p', "Hello Card"),
-            m('p', "Hello Card"),
-            m('p', "Hello Card"),
-            m('p', "Hello Card"),
             m('p', "Hello Card"),
             m('p', "Hello Card"),
         ])
@@ -20,29 +31,11 @@ const List = {
     view: function(vnode) {
         return m('div.list-wrapper', [
             m('div.list-head', [
-              m('p', 'Head')
+                m('p', 'Head')
             ]),
             m('div.list-body', [
                 m(Card),
-                m(Card),
-                m(Card),
-                m(Card),
-                m(Card),
-                m(Card),
-                m(Card),
-                m(Card),
-                m(Card),
-                m(Card),
-                m(Card),
-                m(Card),
-                m(Card),
-                m(Card),
-                m(Card),
-                m(Card),
-                m(Card),
-                m(Card),
-                m(Card),
-                m(Card),
+                m(Card)
             ])
         ])
     }
@@ -50,16 +43,9 @@ const List = {
 
 const Panel = {
     view: function(vnode) {
-        return m('div.panel', [
-            m(List),
-            m(List),
-            m(List),
-            m(List),
-            m(List),
-            m(List),
-            m(List),
-            m(List)
-        ])
+      return m('div.panel', Data.lists.map((list) => {
+        return m(List)
+      }))
     }
 }
 m.route(root, "/", {
