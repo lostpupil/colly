785194609c4bd81152bb397ca32afd5ba8f54527
banana <banana@eurus.cn>
2018-12-08 16:36:43 +0800

    update style

--- src/index.js
+++ src/index.js
@@ -3,54 +3,65 @@ import m from 'mithril'
 const root = document.querySelector('.app')
 
 const Card = {
-  view: function (vnode) {
-    return m('div.card', [
-      m('h1', "Hello Card")
-    ])
-  }
+    view: function(vnode) {
+        return m('div.card', [
+            m('p', "Hello Card"),
+            m('p', "Hello Card"),
+            m('p', "Hello Card"),
+            m('p', "Hello Card"),
+            m('p', "Hello Card"),
+            m('p', "Hello Card"),
+            m('p', "Hello Card"),
+        ])
+    }
 }
 
 const List = {
-  view: function (vnode) {
-    return m('div.list', [
-      m(Card),
-      m(Card),
-      m(Card),
-      m(Card),
-      m(Card),
-      m(Card),
-      m(Card),
-      m(Card),
-      m(Card),
-      m(Card),
-      m(Card),
-      m(Card),
-      m(Card),
-      m(Card),
-      m(Card),
-      m(Card),
-      m(Card),
-      m(Card),
-      m(Card),
-      m(Card),
-    ])
-  }
+    view: function(vnode) {
+        return m('div.list-wrapper', [
+            m('div.list-head', [
+              m('p', 'Head')
+            ]),
+            m('div.list-body', [
+                m(Card),
+                m(Card),
+                m(Card),
+                m(Card),
+                m(Card),
+                m(Card),
+                m(Card),
+                m(Card),
+                m(Card),
+                m(Card),
+                m(Card),
+                m(Card),
+                m(Card),
+                m(Card),
+                m(Card),
+                m(Card),
+                m(Card),
+                m(Card),
+                m(Card),
+                m(Card),
+            ])
+        ])
+    }
 }
 
 const Panel = {
-  view: function (vnode) {
-    return m('div.panel', [
-      m(List),
-      m(List),
-      m(List),
-      m(List),
-      m(List),
-      m(List),
-      m(List),
-      m(List)
-    ])
-  }
+    view: function(vnode) {
+        return m('div.panel', [
+            m(List),
+            m(List),
+            m(List),
+            m(List),
+            m(List),
+            m(List),
+            m(List),
+            m(List)
+        ])
+    }
 }
 m.route(root, "/", {
-  "/": Panel
+    "/": Panel
 })
